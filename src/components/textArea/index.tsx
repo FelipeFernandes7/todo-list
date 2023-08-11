b5278e3920239cba79a59b1679ebeb7f1272bec8
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-11 16:58:25 -0300

    adjust textarea

--- src/components/textArea/index.tsx
+++ src/components/textArea/index.tsx
@@ -1,3 +1,9 @@
-export function TextArea(){
-    return <textarea></textarea>
+import { HTMLProps } from "react";
+import styles from "./styles.module.scss";
+export function TextArea({ ...rest }: HTMLProps<HTMLTextAreaElement>) {
+  return (
+    <>
+      <textarea className={styles.textArea} {...rest}/>
+    </>
+  );
 }
