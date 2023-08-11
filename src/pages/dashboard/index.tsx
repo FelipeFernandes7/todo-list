b2b162d536dbb1417f8e6e1e80daee51fb10fab1
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-11 14:50:22 -0300

    create new components

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,49 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+
+import { TextArea } from "@/components/textArea";
+
+import Head from "next/head";
+
+import styles from "./styles.module.scss";
+
+export default function Dashboard() {
+  <div className={styles.container}>
+    <Head>
+      <title>Meu painel de tarefas</title>
+    </Head>
+    <main className={styles.main}>
+      <section className={styles.content}>
+        <div className={styles.contentForm}>
+          <h1 className={styles.title}>Qual sua tarefa?</h1>
+          <form>
+            <TextArea />
+            <div className={styles.checkboxArea}>
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <label htmlFor="">Deixar tarefa pública</label>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  </div>;
+}
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
