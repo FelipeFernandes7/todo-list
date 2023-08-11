accffabb770dae75dee3a906283b187db26eb55f
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-11 14:49:31 -0300

    create a validation session

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,39 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 import Link from "next/link";
 import styles from "./styles.module.scss";
 export function Header() {
+  const { data: session, status } = useSession();
+  
   return (
     <header className={styles.header}>
       <section className={styles.content}>
         <nav className={styles.nav}>
           <Link href={"/"}>
             <h1 className={styles.logo}>
-              Tarefas <span>+</span>
+              Web<span>Tasks</span>
             </h1>
           </Link>
-          <Link className={styles.links} href={"/dashboard"}>
-            Meu Painel
-          </Link>
+          {session?.user && (
+            <Link className={styles.links} href={"/dashboard"}>
+              Meu Painel
+            </Link>
+          )}
         </nav>
-        <button className={styles.loginButton}>Acessar</button>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button onClick={() => signOut()} className={styles.loginButton}>
+            Olá {session.user?.name}
+          </button>
+        ) : (
+          <button
+            onClick={() => signIn("google")}
+            className={styles.loginButton}
+          >
+            Entrar com Google
+          </button>
+        )}
       </section>
     </header>
   );
