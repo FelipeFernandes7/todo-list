afb6643ea396b5a90102cf27294fd7d5c15209c8
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-11 16:59:22 -0300

    adjusting and add placeholder

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,30 +8,35 @@ import Head from "next/head";
 import styles from "./styles.module.scss";
 
 export default function Dashboard() {
-  <div className={styles.container}>
-    <Head>
-      <title>Meu painel de tarefas</title>
-    </Head>
-    <main className={styles.main}>
-      <section className={styles.content}>
-        <div className={styles.contentForm}>
-          <h1 className={styles.title}>Qual sua tarefa?</h1>
-          <form>
-            <TextArea />
-            <div className={styles.checkboxArea}>
-              <input
-                className={styles.checkbox}
-                type="checkbox"
-                name=""
-                id=""
-              />
-              <label htmlFor="">Deixar tarefa pública</label>
-            </div>
-          </form>
-        </div>
-      </section>
-    </main>
-  </div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+            <form>
+              <TextArea placeholder="Digite sua tarefa" />
+              <div className={styles.checkboxArea}>
+                <input
+                  className={styles.checkbox}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <label htmlFor="">Deixar tarefa pública</label>
+              </div>
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
